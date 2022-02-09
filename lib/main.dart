631e306bf4986014ef10200c9cab03ceb259c8de
David Ruiz <53538743+davidlruizc@users.noreply.github.com>
2022-02-08 21:19:20 -0500

    add connection with the description widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:journey_with_me/description-place.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Journey Me',
       theme: ThemeData(
@@ -24,7 +29,12 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.cyan,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Journey With Me"),
+        ),
+        body: DescriptionPlace(namePlace: "Bahamas", stars: 4, descriptionPlace: descriptionDummy)
+      ),
     );
   }
 }
